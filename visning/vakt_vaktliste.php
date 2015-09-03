b5440d4be659c06a55a35853088e5fbacb0489ab
Martin N <derp@derp.de>
2015-09-03 23:09:26 +0200

    Datofeilfiks i vaktlista.

--- visning/vakt_vaktliste.php
+++ visning/vakt_vaktliste.php
@@ -7,18 +7,21 @@ require_once('topp.php');
 	<h1>Vaktliste</h1>
 <?php
 
+$ukeStart = strtotime('last sunday - 6 days, midnight');
+
 foreach (range($denneUka, $denneUka > 26 ? 52 : 26) as $uke){
+	$ukeStart = strtotime('+1 week', $ukeStart);
 ?>
 	<table class="table-bordered table">
 		<tr>
 			<th><span class="hidden-xs">U<span class="hidden-sm">ke</span>&nbsp;</span><?php echo $uke; ?></th>
-			<th>M<span class="hidden-xs">an<span class="hidden-sm">dag</span></span>&nbsp;<?php echo date('d/m', strtotime("+ ".($uke - $denneUka)." week"));?></th>
-			<th>T<span class="hidden-xs">ir<span class="hidden-sm">sdag</span></span>&nbsp;<?php echo date('d/m', strtotime("+ ".($uke - $denneUka)." week +1 day"));?></th>
-			<th>O<span class="hidden-xs">ns<span class="hidden-sm">dag</span></span>&nbsp;<?php echo date('d/m', strtotime("+ ".($uke - $denneUka)." week +2 day"));?></th>
-			<th>T<span class="hidden-xs">or<span class="hidden-sm">sdag</span></span>&nbsp;<?php echo date('d/m', strtotime("+ ".($uke - $denneUka)." week +3 day"));?></th>
-			<th>F<span class="hidden-xs">re<span class="hidden-sm">dag</span></span>&nbsp;<?php echo date('d/m', strtotime("+ ".($uke - $denneUka)." week +4 day"));?></th>
-			<th>L<span class="hidden-xs">ør<span class="hidden-sm">dag</span></span>&nbsp;<?php echo date('d/m', strtotime("+ ".($uke - $denneUka)." week +5 day"));?></th>
-			<th>S<span class="hidden-xs">øn<span class="hidden-sm">dag</span></span>&nbsp;<?php echo date('d/m', strtotime("+ ".($uke - $denneUka)." week +6 day"));?></th>
+			<th>M<span class="hidden-xs">an<span class="hidden-sm">dag</span></span>&nbsp;<?php echo date('d/m',  $ukeStart);?></th>
+			<th>T<span class="hidden-xs">ir<span class="hidden-sm">sdag</span></span>&nbsp;<?php echo date('d/m', strtotime('+1 day', $ukeStart));?></th>
+			<th>O<span class="hidden-xs">ns<span class="hidden-sm">dag</span></span>&nbsp;<?php echo date('d/m', strtotime('+2 day', $ukeStart));?></th>
+			<th>T<span class="hidden-xs">or<span class="hidden-sm">sdag</span></span>&nbsp;<?php echo date('d/m', strtotime('+3 day', $ukeStart));?></th>
+			<th>F<span class="hidden-xs">re<span class="hidden-sm">dag</span></span>&nbsp;<?php echo date('d/m', strtotime('+4 day', $ukeStart));?></th>
+			<th>L<span class="hidden-xs">ør<span class="hidden-sm">dag</span></span>&nbsp;<?php echo date('d/m', strtotime('+5 day', $ukeStart));?></th>
+			<th>S<span class="hidden-xs">øn<span class="hidden-sm">dag</span></span>&nbsp;<?php echo date('d/m', strtotime('+6 day', $ukeStart));?></th>
 		</tr>
 <?php
 	foreach (range(1, 4) as $vakttype){
