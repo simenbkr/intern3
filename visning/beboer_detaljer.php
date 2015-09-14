9d7c39a3703aa7772bdad37a98203a8f67022a40
marksome <kukfaen@gauder.no>
2015-09-14 11:05:14 +0200

    La til tabell i beboer_detaljer.

--- visning/beboer_detaljer.php
+++ visning/beboer_detaljer.php
@@ -6,14 +6,17 @@ require_once('topp.php');
 
 <div class="col-md-12">
 	<h1><?php echo $beboer->getFulltNavn(); ?></h1>
-	<table>
-		<tr>
-			<td>Homo</td>
-			<td>Jeg var bøg for seriøst!</td>
-			<td>Hva liker do å gjøre? Pule?</td>
-			<td>Ensom? Pule? #261</td>
-		</tr>
-	</table>
+	<div>
+		<table>
+			<tr>
+				<td>Homo</td>
+				<td>Jeg var bøg for seriøst!</td>
+				<td>Hva liker do å gjøre? Pule?</td>
+				<td>Ensom? Pule? #261</td>
+			</tr>
+		</table>
+	</div>
+	<p></p>
 	<p>Masse kule ting i en tabell hadde gjordt seg her. F.eks. bilde også.</p>
 </div>
 
