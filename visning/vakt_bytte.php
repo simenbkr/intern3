683bf699adaf51c22f48b1c5d594a485d9a489a1
marksome <marksome@singel.singsaker.no>
2015-09-21 20:47:11 +0200

    La til dotter på forsiden og startet på utseendet til vakt_bytter.php

--- visning/vakt_bytte.php
+++ visning/vakt_bytte.php
@@ -3,10 +3,47 @@
 require_once('topp.php');
 
 ?>
+
 <div class="col-md-12">
 	<h1>Vakt &raquo; Vaktbytte</h1>
 	<p>[ <a href="?a=vakt">Vaktliste</a> ] [ Vaktbytte ]</p>
 </div>
+<div class="col-md-10 col-sm-6">
+	<table class="table table-bordered">
+		<tr>
+			<th>1. Vakt</th>
+			<th>2. Vakt</th>
+			<th>3. Vakt</th>
+			<th>4. Vakt</th>
+		</tr>
+		<tr>
+<?php
+
+// foreach ($vaktbytte as $byttevakt) {
+//
+// }
+
+?>
+			<td>Søndag 27/09<br>Gauder<INPUT TYPE= "Submit" STYLE="float:right" VALUE="Bytt"></td>
+			<td>Søndag 29/09<br>Gauder<INPUT TYPE= "Submit" STYLE="float:right" VALUE="Bytt"></td>
+			<td>Søndag 09/10<br>Gauder<INPUT TYPE= "Submit" STYLE="float:right" VALUE="Bytt"></td>
+			<td>Søndag 03/11<br>Gauder<INPUT TYPE= "Submit" STYLE="float:right" VALUE="Bytt"></td>
+		</tr>
+		<tr>
+			<td>Søndag 28/09<br>Gauder<INPUT TYPE= "Submit" STYLE="float:right" VALUE="Bytt"></td>
+			<td>Søndag 05/10<br>Gauder<INPUT TYPE= "Submit" STYLE="float:right" VALUE="Bytt"></td>
+			<td>Søndag 10/10<br>Gauder<INPUT TYPE= "Submit" STYLE="float:right" VALUE="Bytt"></td>
+			<td>Søndag 10/10<br>Gauder<INPUT TYPE= "Submit" STYLE="float:right" VALUE="Bytt"></td>
+		</tr>
+		<tr>
+			<td>Søndag 26/10<br>Gauder<INPUT TYPE= "Submit" STYLE="float:right" VALUE="Bytt"></td>
+			<td>Søndag 03/11<br>Gauder<INPUT TYPE= "Submit" STYLE="float:right" VALUE="Bytt"></td>
+			<td>Søndag 06/11<br>Gauder<INPUT TYPE= "Submit" STYLE="float:right" VALUE="Bytt"></td>
+			<td>Søndag 09/11<br>Gauder<INPUT TYPE= "Submit" STYLE="float:right" VALUE="Bytt"></td>
+		</tr>
+	</table>
+</div>
+
 <?php
 
 require_once('bunn.php');
