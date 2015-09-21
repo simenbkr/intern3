4f9789d3e7a47c50de7950b5d11217d1c4330420
marksome <marksome@singel.singsaker.no>
2015-09-21 17:42:35 +0200

    La til filerne Vaktbytte.php og vakt_bytte.php

---
+++ modell/Vaktbytte.php
@@ -0,0 +1,61 @@
+<?php
+
+namespace intern3;
+
+class Vaktbytte {
+
+	private $id;
+	private $vaktId;
+	private $gisBort;
+	private $forslag;
+	private $merknad;
+
+	public static function medId($id) {
+		$st = DB::getDB()->prepare('SELECT * FROM vaktbytte WHERE id=:id;');
+		$st->bindParam(':id', $id);
+		$st->execute();
+		return self::init($st);
+	}
+	public static function medVaktId($vaktId) {
+		$st = DB::getDB()->prepare('SELECT * FROM vaktbytte WHERE vakt_id=:vaktId;');
+		$st->bindParam(':vaktId', $vaktId);
+		$st->execute();
+		return self::init($st);
+	}
+	private static function init(\PDOStatement $st) {
+		$rad = $st->fetch();
+		if ($rad == null) {
+			return null;
+		}
+		$instance = new self();
+		$instance->id = $rad['id'];
+		$instance->vaktId = $rad['vakt_id'];
+		$instance->gisBort = $rad['gisbort'];
+		$instance->forslag = $rad['forslag'];
+		$instance->merknad = $rad['merknad'];
+		return $instance;
+	}
+
+	public function getId() {
+		return $this->id;
+	}
+
+	public function getVaktId() {
+		return $this->vaktId;
+	}
+
+	public function getGisBort() {
+		return $this->gisBort;
+	}
+
+	// public function getForslag() {
+	// 	return $this->forslag;
+	// }
+
+	public function getMerknad() {
+		return $this->merknad;
+	}
+
+}
+
+?>
---
+++ visning/vakt_bytte.php
@@ -0,0 +1,14 @@
+<?php
+
+require_once('topp.php');
+
+?>
+<div class="col-md-12">
+	<h1>Vakt &raquo; Vaktbytte</h1>
+	<p>[ <a href="?a=vakt">Vaktliste</a> ] [ Vaktbytte ]</p>
+</div>
+<?php
+
+require_once('bunn.php');
+
+?>
