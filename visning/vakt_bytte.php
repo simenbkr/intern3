6db28f011466db20d6cc4f1a805184686da8eead
marksome <marksome@singel.singsaker.no>
2015-10-15 18:14:50 +0200

    Lagde modal på vaktbytte.

--- visning/vakt_bytte.php
+++ visning/vakt_bytte.php
@@ -9,34 +9,43 @@ require_once('topp.php');
 	<p>[ <a href="?a=vakt">Vaktliste</a> ] [ Vaktbytte ]</p>
 </div>
 <div class="col-md-3 col-sm-6 col-sx-12">
-  <table class="table table-bordered">
-    <tr>
-      <th>Dine vakter</th>
-    </tr>
 <?php
-
-foreach (intern3\VaktListe::medBrukerId($cd->getAktivBruker()->getId()) as $vakt) {
-  $tid = strtotime($vakt->getDato());
-  $tekst = $vakt->getVakttype() . '. vakt ' . strftime('%A %d/%m', $tid);
-  ?>    <tr>
-<?php
-if ($vakt->erFerdig()) {
+function visDineVakter($visFerdig = true) {
+  global $cd;
   ?>
-      <td class="celle_graa"><?php echo $tekst; ?></td>
-<?php
-}
-else {
-  ?>      <td><?php echo $tekst; ?>
-        <input class="btn btn-info pull-right" type="button" value="Bytt">
-      </td>
-<?php
-}
-?>
-    </tr>
-<?php
+    <table class="table table-bordered">
+      <tr>
+        <th>Dine vakter</th>
+      </tr>
+  <?php
+  foreach (intern3\VaktListe::medBrukerId($cd->getAktivBruker()->getId()) as $vakt) {
+    $tid = strtotime($vakt->getDato());
+    $tekst = $vakt->getVakttype() . '. vakt ' . strftime('%A %d/%m', $tid);
+    ?>    <tr>
+  <?php
+  if ($vakt->erFerdig()) {
+    if ($visFerdig) {
+      ?>
+          <td class="celle_graa"><?php echo $tekst; ?></td>
+    <?php
+    }
   }
+  else {
+    ?>      <td><?php echo $tekst; ?>
+          <input class="btn btn-info pull-right" type="button" value="Bytt">
+        </td>
+  <?php
+  }
+  ?>
+      </tr>
+  <?php
+    }
+  ?>
+    </table>
+<?php
+}
+visDineVakter();
 ?>
-  </table>
 </div>
 <div class="col-md-12"> </div>
 <?php
@@ -49,12 +58,35 @@ foreach (range(1, 4) as $vakttype) {
 		</tr>
 <?php
 	foreach ($vaktbytteListe[$vakttype] as $vaktbytte) {
+    $modalId = 'modal-' . date('m-d', strtotime($vaktbytte->getVakt()->getDato())) . '-' . $vaktbytte->getVakt()->getVakttype();
 		?>		<tr>
-			<td><input type="button" class="btn btn-info pull-right" value="Bytt"><?php
-		echo '<strong>' . ucfirst(strftime('%A %d/%m', strtotime($vaktbytte->getVakt()->getDato()))) . '</strong>';
-		echo '<br>';
+			<td>
+<?php
+    if ($vaktbytte->getVakt()->getBrukerId() != $cd->getAktivBruker()->getId()) {
+      echo '        <input type="button" class="btn btn-info pull-right" value="Bytt" data-toggle="modal" data-target="#' . $modalId . '">' . PHP_EOL;
+    }
+		echo '        <strong>' . ucfirst(strftime('%A %d/%m', strtotime($vaktbytte->getVakt()->getDato()))) . '</strong>' . PHP_EOL;
+		echo '        <br>' . PHP_EOL;
 		echo $vaktbytte->getVakt()->getBruker()->getPerson()->getFulltNavn();
-		?></td>
+?>
+        <div class="modal fade" id="<?php echo $modalId; ?>" role="dialog">
+          <div class="modal-dialog modal-sm">
+            <div class="modal-content">
+              <div class="modal-header">
+                <button type="button" class="close" data-dismiss="modal">&times;</button>
+                <h4 class="modal-title"><?php echo $vaktbytte->getVakt()->getVakttype() . '. vakt ' . strftime('%A %d/%m', strtotime($vaktbytte->getVakt()->getDato())); ?></h4>
+              </div>
+              <div class="modal-body">
+                <p>Hvilken vakt vil du foreslå å bytte?</p>
+                <?php visDineVakter(false); ?>
+              </div>
+              <div class="modal-footer">
+                <button type="button" class="btn btn-default" data-dismiss="modal">Lukk</button>
+              </div>
+            </div>
+          </div>
+        </div>
+      </td>
 		</tr><?php
 	}
 	?>	</table>
