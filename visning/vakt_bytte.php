78bf5f409c711977517621adfd8b008c075b4f04
marksome <marksome@singel.singsaker.no>
2015-10-15 17:26:08 +0200

    Har pynta på vaktbytte og lagt til "Dine vakter".

--- visning/vakt_bytte.php
+++ visning/vakt_bytte.php
@@ -8,6 +8,37 @@ require_once('topp.php');
 	<h1>Vakt &raquo; Vaktbytte</h1>
 	<p>[ <a href="?a=vakt">Vaktliste</a> ] [ Vaktbytte ]</p>
 </div>
+<div class="col-md-3 col-sm-6 col-sx-12">
+  <table class="table table-bordered">
+    <tr>
+      <th>Dine vakter</th>
+    </tr>
+<?php
+
+foreach (intern3\VaktListe::medBrukerId($cd->getAktivBruker()->getId()) as $vakt) {
+  $tid = strtotime($vakt->getDato());
+  $tekst = $vakt->getVakttype() . '. vakt ' . strftime('%A %d/%m', $tid);
+  ?>    <tr>
+<?php
+if ($vakt->erFerdig()) {
+  ?>
+      <td class="celle_graa"><?php echo $tekst; ?></td>
+<?php
+}
+else {
+  ?>      <td><?php echo $tekst; ?>
+        <input class="btn btn-info pull-right" type="button" value="Bytt">
+      </td>
+<?php
+}
+?>
+    </tr>
+<?php
+  }
+?>
+  </table>
+</div>
+<div class="col-md-12"> </div>
 <?php
 
 foreach (range(1, 4) as $vakttype) {
@@ -19,7 +50,7 @@ foreach (range(1, 4) as $vakttype) {
 <?php
 	foreach ($vaktbytteListe[$vakttype] as $vaktbytte) {
 		?>		<tr>
-			<td><input type="button" class="pull-right" value="Bytt"><?php
+			<td><input type="button" class="btn btn-info pull-right" value="Bytt"><?php
 		echo '<strong>' . ucfirst(strftime('%A %d/%m', strtotime($vaktbytte->getVakt()->getDato()))) . '</strong>';
 		echo '<br>';
 		echo $vaktbytte->getVakt()->getBruker()->getPerson()->getFulltNavn();
