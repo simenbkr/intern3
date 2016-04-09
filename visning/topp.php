f4e45ddfbff164b34829fbba8c40bd3284d99348
Martin N <derp@derp.de>
2016-04-09 15:26:54 +0200

    Endra datoformat etter iso-standard

--- visning/topp.php
+++ visning/topp.php
@@ -11,7 +11,7 @@
 		<link rel="stylesheet" href="css/jquery-ui.css">
 		<script>
 var formaterDatovelger = function() {
-  $('.datepicker').datepicker({dateFormat: "dd/mm/yy"});
+  $('.datepicker').datepicker({dateFormat: "yy-mm-dd"});
 };
 $(formaterDatovelger);
 		</script>
