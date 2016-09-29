bcad64e74c1b7e30853bfbf2b6a976d4242cb6ee
simenbkr <simen@haitech.org>
2016-09-29 14:32:35 +0200

    Ordnet godkjenn/fjern funk for oppgave. Må ordne med navn/godkjentav-tingen i denne

--- modell/Oppgave.php
+++ modell/Oppgave.php
@@ -2,187 +2,222 @@
 
 namespace intern3;
 
-class Oppgave {
-	private $id;
-	private $tid_oppretta;
-	private $anslag_timer;
-	private $ansvarsomrade_id; // trengs bare hvis oppgaven ikke tilhører noen rapport
-	private $anslag_personer;
-	private $prioritet_id;
-	private $navn;
-	private $beskrivelse;
-	private $godkjent;
-	private $tid_godkjent;
-	private $godkjent_bruker_id;
+class Oppgave
+{
+    private $id;
+    private $tid_oppretta;
+    private $anslag_timer;
+    private $ansvarsomrade_id; // trengs bare hvis oppgaven ikke tilhører noen rapport
+    private $anslag_personer;
+    private $prioritet_id;
+    private $navn;
+    private $beskrivelse;
+    private $godkjent;
+    private $tid_godkjent;
+    private $godkjent_bruker_id;
 
-	private $db;
+    private $db;
 
-	//latskapsinstansiering
-	private $pameldte;
-	private $arbeidListe; //arbeidListe er flertallsformen av arbeid
-	private $arbeidListeBrukerId;
+    //latskapsinstansiering
+    private $pameldte;
+    private $arbeidListe; //arbeidListe er flertallsformen av arbeid
+    private $arbeidListeBrukerId;
 
-	public function __construct () {
-		$this->db = DB::getDB();
-	}
-	private function init($pdo_statement)
-	{
-		$st = $pdo_statement;
-		$st->execute();
-		if($st->rowCount() > 0)
-		{
-			$rad = $st->fetch();
+    public function __construct()
+    {
+        $this->db = DB::getDB();
+    }
 
-			$this->id                 = $rad['id'];
-			$this->tid_oppretta       = $rad['tid_oppretta'];
-			$this->anslag_timer       = $rad['anslag_timer'];
-			$this->ansvarsomrade_id   = $rad['ansvarsomrade_id'];
-			$this->anslag_personer    = $rad['anslag_personer'];
-			$this->prioritet_id       = $rad['prioritet_id'];
-			$this->navn               = $rad['navn'];
-			$this->beskrivelse        = $rad['beskrivelse'];
-			$this->godkjent           = $rad['godkjent'];
-			$this->tid_godkjent       = $rad['tid_godkjent'];
-			$this->godkjent_bruker_id = $rad['godkjent_bruker_id'];
+    private function init($pdo_statement)
+    {
+        $st = $pdo_statement;
+        $st->execute();
+        if ($st->rowCount() > 0) {
+            $rad = $st->fetch();
 
-			$this->brukere = null;
-		}
-	}
-	public static function medId($id)
-	{
-		$instans = new self();
-		$st = $instans->db->prepare('SELECT * FROM oppgave WHERE id = :id');
-		$st->bindParam(":id" , $id);
+            $this->id = $rad['id'];
+            $this->tid_oppretta = $rad['tid_oppretta'];
+            $this->anslag_timer = $rad['anslag_timer'];
+            $this->ansvarsomrade_id = $rad['ansvarsomrade_id'];
+            $this->anslag_personer = $rad['anslag_personer'];
+            $this->prioritet_id = $rad['prioritet_id'];
+            $this->navn = $rad['navn'];
+            $this->beskrivelse = $rad['beskrivelse'];
+            $this->godkjent = $rad['godkjent'];
+            $this->tid_godkjent = $rad['tid_godkjent'];
+            $this->godkjent_bruker_id = $rad['godkjent_bruker_id'];
 
-		$instans->init($st);
-		return $instans;
-	}
+            $this->brukere = null;
+        }
+    }
 
-	public function getId () {
-		return $this->id;
-	}
+    public static function medId($id)
+    {
+        $instans = new self();
+        $st = $instans->db->prepare('SELECT * FROM oppgave WHERE id = :id');
+        $st->bindParam(":id", $id);
 
-	public function getTidOppretta () {
-		return $this->tid_oppretta;
-	}
+        $instans->init($st);
+        return $instans;
+    }
 
-	public function getAnslagTimer () {
-		return $this->anslag_timer;
-	}
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	public function getAnsvarsomradeId () {
-		return $this->ansvarsomrade_id;
-	}
+    public function getTidOppretta()
+    {
+        return $this->tid_oppretta;
+    }
 
-	public function getAnslagPersoner () {
-		return $this->anslag_personer;
-	}
+    public function getAnslagTimer()
+    {
+        return $this->anslag_timer;
+    }
 
-	public function getNavn () {
-		return $this->navn;
-	}
+    public function getAnsvarsomradeId()
+    {
+        return $this->ansvarsomrade_id;
+    }
 
-	public function getBeskrivelse () {
-		return $this->beskrivelse;
-	}
+    public function getAnslagPersoner()
+    {
+        return $this->anslag_personer;
+    }
 
-	public function getGodkjent () {
-		return $this->godkjent;
-	}
+    public function getNavn()
+    {
+        return $this->navn;
+    }
 
-	public function getTidGodkjent () {
-		return $this->tid_godkjent;
-	}
+    public function getBeskrivelse()
+    {
+        return $this->beskrivelse;
+    }
 
-	public function getGodkjentBrukerId () {
-		return $this->godkjent_bruker_id;
-	}
+    public function getGodkjent()
+    {
+        return $this->godkjent;
+    }
 
-	public function getGodkjentBruker()
-	{
-		return Person::medId($this->godkjent_bruker_id);
-	}
+    public function getTidGodkjent()
+    {
+        return $this->tid_godkjent;
+    }
 
-	public function getPrioritet()
-	{
-		return Prioritet::medId($this->getPrioritetId());
-	}
+    public function getGodkjentBrukerId()
+    {
+        return $this->godkjent_bruker_id;
+    }
 
-	public static function setGodkjent($oppgaver_id, $godkjent) {
-		$instans = new self(); //BUG: Får ikke tak i $this->db, så hva skal vi med $this->db ?
-		foreach ($oppgaver_id as $oppgave_id) {
-			$st = $instans->db->prepare('UPDATE oppgave SET godkjent=:godkjent,tid_godkjent=NOW(),godkjent_bruker_id=:bruker_id WHERE godkjent=' . (1^$godkjent) . ' AND id = :rapport_id;');
-			//Faen ta PDO: Kan ikke skrive "WHERE godkjent=:motsatt".
+    public function getGodkjentBruker()
+    {
+        return Person::medId($this->godkjent_bruker_id);
+    }
 
-			$st->bindParam(':godkjent'   , $godkjent);
-			$st->bindParam(':rapport_id' , $rapport_id);
-			$st->bindParam(":bruker_id"  , $_SESSION['bruker_id']);
-			$st->execute();
-		}
-	}
+    public function getPrioritet()
+    {
+        return Prioritet::medId($this->getPrioritetId());
+    }
 
-	public function setBrukerPameldt ($bruker_id, $pameldt) {
-		$instans = new self();
-		$sql = ($pameldt) ?
-			'INSERT INTO oppgave_bruker(bruker_id,oppgave_id) VALUES(:brk,:oppg)'
-		:
-			'DELETE FROM oppgave_bruker WHERE bruker_id=:brk AND oppgave_id=:oppg'
-		;
-		$st = $instans->db->prepare($sql);
-		$st->bindParam(':brk', $bruker_id);
-		$st->bindParam(':oppg', $this->id);
-		$st->execute();
-	}
+    public static function setGodkjent($oppgaver_id, $godkjent)
+    {
+        $instans = new self(); //BUG: Får ikke tak i $this->db, så hva skal vi med $this->db ?
+        foreach ($oppgaver_id as $oppgave_id) {
+            $st = $instans->db->prepare('UPDATE oppgave SET godkjent=:godkjent,tid_godkjent=NOW(),godkjent_bruker_id=:bruker_id WHERE godkjent=' . (1 ^ $godkjent) . ' AND id = :rapport_id;');
+            //Faen ta PDO: Kan ikke skrive "WHERE godkjent=:motsatt".
 
-	/*
-	 * Funksjonene nedenfor instansierer variabler ved behov.
-	 */
-	public function getPameldte () {
-		if(!$this->pameldte){
-			// lazyinit
-			$this->pameldte = PersonListe::medOppgaveId($this->id);
-		}
-		return $this->pameldte;
-	}
+            $st->bindParam(':godkjent', $godkjent);
+            $st->bindParam(':rapport_id', $rapport_id);
+            $st->bindParam(":bruker_id", $_SESSION['bruker_id']);
+            $st->execute();
+        }
+    }
 
-	public function getArbeidListe () {
-		if(!$this->arbeidListe){
-			// lazyinit
-			$this->arbeidListe = ArbeidListe::medOppgaveId($this->id);
-		}
-		return $this->arbeidListe;
-	}
+    public function setBrukerPameldt($bruker_id, $pameldt)
+    {
+        $instans = new self();
+        $sql = ($pameldt) ?
+            'INSERT INTO oppgave_bruker(bruker_id,oppgave_id) VALUES(:brk,:oppg)'
+            :
+            'DELETE FROM oppgave_bruker WHERE bruker_id=:brk AND oppgave_id=:oppg';
+        $st = $instans->db->prepare($sql);
+        $st->bindParam(':brk', $bruker_id);
+        $st->bindParam(':oppg', $this->id);
+        $st->execute();
+    }
 
-	public function getArbeidListeBrukerId ($bruker_id) {
-		if(!$this->arbeidListeBrukerId){
-			// lazyinit
-			$this->arbeidListeBrukerId = ArbeidListe::medBrukerIdOppgIdVelg($bruker_id, $this->id, ArbeidPolymorfkategori::OPPG);
-		}
-		return $this->arbeidListeBrukerId;
-	}
+    public static function endreGodkjent($oppgaveId, $status){
+        if($status == 0){
+            $st = DB::getDB()->prepare('UPDATE oppgave SET godkjent=0,tid_godkjent=NULL,godkjent_bruker_id=NULL WHERE id=:id');
+            $st->bindParam(':id',$oppgaveId);
+            $st->execute();
+        }
+        elseif($status == 1) {
+            $st = DB::getDB()->prepare('UPDATE oppgave SET godkjent=1,tid_godkjent=NOW(),godkjent_bruker_id=:brukerId WHERE id=:id');
+            $st->bindParam(':id',$oppgaveId);
+            $aktivbrukerId = LogginnCtrl::getAktivBruker()->getId();
+            $st->bindParam(':brukerId',$aktivbrukerId);
+            $st->execute();
+        }
+    }
 
-	public function erBrukerPameldt ($bruker_id) {
-		$this->getPameldte();
-		foreach($this->pameldte as $bruker){
-			if($bruker->getId() == $bruker_id){
-				return true;
-			}
-		}
-		return false;
-	}
-	public function getPrioritetId () {
-		if(!$this->prioritet_id){
-			// lazyinit
-			$pri = array();
-			foreach (RapportListe::medOppgaveId($this->id) as $Rapport) {
-			$pri[$Rapport->getPrioritetId()] = $Rapport->getPrioritet().getNummer();
-			}
-			arsort($pri , SORT_NUMERIC);
-			$pri = array_keys($pri);
-			$this->prioritet_id = $pri[0];
-		}
-		return $this->prioritet_id;
-	}
+    /*
+     * Funksjonene nedenfor instansierer variabler ved behov.
+     */
+    public function getPameldte()
+    {
+        if (!$this->pameldte) {
+            // lazyinit
+            $this->pameldte = PersonListe::medOppgaveId($this->id);
+        }
+        return $this->pameldte;
+    }
+
+    public function getArbeidListe()
+    {
+        if (!$this->arbeidListe) {
+            // lazyinit
+            $this->arbeidListe = ArbeidListe::medOppgaveId($this->id);
+        }
+        return $this->arbeidListe;
+    }
+
+    public function getArbeidListeBrukerId($bruker_id)
+    {
+        if (!$this->arbeidListeBrukerId) {
+            // lazyinit
+            $this->arbeidListeBrukerId = ArbeidListe::medBrukerIdOppgIdVelg($bruker_id, $this->id, ArbeidPolymorfkategori::OPPG);
+        }
+        return $this->arbeidListeBrukerId;
+    }
+
+    public function erBrukerPameldt($bruker_id)
+    {
+        $this->getPameldte();
+        foreach ($this->pameldte as $bruker) {
+            if ($bruker->getId() == $bruker_id) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    public function getPrioritetId()
+    {
+        if (!$this->prioritet_id) {
+            // lazyinit
+            $pri = array();
+            foreach (RapportListe::medOppgaveId($this->id) as $Rapport) {
+                $pri[$Rapport->getPrioritetId()] = $Rapport->getPrioritet() . getNummer();
+            }
+            arsort($pri, SORT_NUMERIC);
+            $pri = array_keys($pri);
+            $this->prioritet_id = $pri[0];
+        }
+        return $this->prioritet_id;
+    }
 }
 
 ?>
--- visning/utvalg_regisjef_oppgave.php
+++ visning/utvalg_regisjef_oppgave.php
@@ -5,130 +5,168 @@ require_once('topp_utvalg.php');
 ?>
 
 <div class="col-md-12">
-	<h1>Utvalget &raquo; Regisjef &raquo; Oppgave</h1>
+    <h1>Utvalget &raquo; Regisjef &raquo; Oppgave</h1>
 </div>
 
 <script>
-function fjern(id) {
-	$.ajax({
-		type: 'POST',
-		url: '<?php echo $_SERVER['REQUEST_URI']; ?>',
-		data: id, // TODO må fikses!
-		success: function(data) {
-			$('#oppgave_' + id).html(data);
-		},
-		error: function(req, stat, err) {
-			alert(err);
-		}
-	});
-}
+    function fjern(id) {
+        $.ajax({
+            type: 'POST',
+            url: '?a=utvalg/regisjef/oppgave/',
+            data: 'fjern=' + id,
+            method: 'POST',
+            success: function (data) {
+                $('#oppgave_' + id).html(data);
+                location.reload();
+            },
+            error: function (req, stat, err) {
+                alert(err);
+            }
+        });
+    }
 
-function godkjenn(id) {
-	$.ajax({
-		type: 'POST',
-		url: '<?php echo $_SERVER['REQUEST_URI']; ?>',
-		data: id, // TODO må fikses!
-		success: function(data) {
-			$('#oppgave_' + id).html(data);
-		},
-		error: function(req, stat, err) {
-			alert(err);
-		}
-	});
-}
+    function godkjenn(id) {
+        $.ajax({
+            type: 'POST',
+            url: '?a=utvalg/regisjef/oppgave/',
+            data: 'godkjenn=' + id,
+            method: 'POST',
+            success: function (data) {
+                $('#oppgave_' + id).html(data);
+                location.reload();
+            },
+            error: function (req, stat, err) {
+                alert(err);
+            }
+        });
+    }
 
-function byttPolymorfkategori(id) {
-	$('#polymorfkategori_ymse').hide();
-	$('#polymorfkategori_feil').hide();
-	$('#polymorfkategori_rapp').hide();
-	$('#polymorfkategori_oppg').hide();
-	switch (id) {
-		case 'ymse':
-			$('#polymorfkategori_ymse').show();
-			break;
-		case 'feil':
-			$('#polymorfkategori_feil').show();
-			break;
-		case 'rapp':
-			$('#polymorfkategori_rapp').show();
-			break;
-		case 'oppg':
-			$('#polymorfkategori_oppg').show();
-			break;
-	}
-}
-$(document).ready(function() {
-	byttPolymorfkategori('<?php echo isset($_POST['polymorfkategori_velger']) ? $_POST['polymorfkategori_velger'] : 'ymse'; ?>');
-});
+    function byttPolymorfkategori(id) {
+        $('#polymorfkategori_ymse').hide();
+        $('#polymorfkategori_feil').hide();
+        $('#polymorfkategori_rapp').hide();
+        $('#polymorfkategori_oppg').hide();
+        switch (id) {
+            case 'ymse':
+                $('#polymorfkategori_ymse').show();
+                break;
+            case 'feil':
+                $('#polymorfkategori_feil').show();
+                break;
+            case 'rapp':
+                $('#polymorfkategori_rapp').show();
+                break;
+            case 'oppg':
+                $('#polymorfkategori_oppg').show();
+                break;
+        }
+    }
+    $(document).ready(function () {
+        byttPolymorfkategori('<?php echo isset($_POST['polymorfkategori_velger']) ? $_POST['polymorfkategori_velger'] : 'ymse'; ?>');
+    });
 
 </script>
 
 <div class="col-md-6 col-sm-12">
-	<form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
-		<table class="table table-bordered">
-      <tr>
-        <th>Navn</th>
-        <td><input name="navn" <?php echo isset($_POST['navn']) ? ' value="' . $_POST['navn'] . '"' : ''; ?>></td>
-      </tr>
-      <tr>
-        <th>Prioritet</th>
-        <td><input name="prioritet" <?php echo isset($_POST['prioritet']) ? ' value="' . $_POST['prioritet'] . '"' : ''; ?>></td>
-      </tr>
-			<tr>
-				<th>Anslag timer</th>
-				<td><input name="timer" placeholder="0:00"<?php echo isset($_POST['timer']) ? ' value="' . $_POST['timer'] . '"' : ''; ?>></td>
-			</tr>
-      <tr>
-        <th>Anslag personer</th>
-        <td><input name="personer" <?php echo isset($_POST['personer']) ? ' value="' . $_POST['personer'] . '"' : ''; ?>></td>
-      </tr>
-			<tr>
-				<th>Beskrivelse</th>
-				<td><textarea name="beskrivelse" cols="50" rows="5"><?php echo isset($_POST['beskrivelse']) ? $_POST['beskrivelse'] : ''; ?></textarea></td>
-			</tr>
-			<tr>
-				<td> </td>
-				<td><input type="submit" class="btn btn-primary" name="registrer" value="Registrer"></td>
-			</tr>
-		</table>
-	</form>
+    <form action="<?php echo $_SERVER['REQUEST_URI']; ?>" method="post">
+        <table class="table table-bordered">
+            <tr>
+                <th>Navn</th>
+                <td><input name="navn" <?php echo isset($_POST['navn']) ? ' value="' . $_POST['navn'] . '"' : ''; ?>>
+                </td>
+            </tr>
+            <tr>
+                <th>Prioritet</th>
+                <td><input
+                        name="prioritet" <?php echo isset($_POST['prioritet']) ? ' value="' . $_POST['prioritet'] . '"' : ''; ?>>
+                </td>
+            </tr>
+            <tr>
+                <th>Anslag timer</th>
+                <td><input name="timer"
+                           placeholder="0:00"<?php echo isset($_POST['timer']) ? ' value="' . $_POST['timer'] . '"' : ''; ?>>
+                </td>
+            </tr>
+            <tr>
+                <th>Anslag personer</th>
+                <td><input
+                        name="personer" <?php echo isset($_POST['personer']) ? ' value="' . $_POST['personer'] . '"' : ''; ?>>
+                </td>
+            </tr>
+            <tr>
+                <th>Beskrivelse</th>
+                <td><textarea name="beskrivelse" cols="50"
+                              rows="5"><?php echo isset($_POST['beskrivelse']) ? $_POST['beskrivelse'] : ''; ?></textarea>
+                </td>
+            </tr>
+            <tr>
+                <td></td>
+                <td><input type="submit" class="btn btn-primary" name="registrer" value="Registrer"></td>
+            </tr>
+        </table>
+    </form>
 </div>
 
 <div class="col-md-12 table-responsive">
-	<table class="table table-striped table-hover">
-		<thead>
-			<tr>
-        <th>Navn</th>
-        <th>Prioritet</th>
-				<th>Anslag timer</th>
-				<th>Anslag personer</th>
-        <th>Påmeldte</th>
-        <th>Beskrivelse</th>
-        <th>Opprettet</th>
-        <th>Godkjent</th>
-				<th>Godkjenn/Fjern</th>
-			</tr>
-		</thead>
-		<tbody>
-<?php
+    <table class="table table-striped table-hover">
+        <thead>
+        <tr>
+            <th>Navn</th>
+            <th>Prioritet</th>
+            <th>Anslag timer</th>
+            <th>Anslag personer</th>
+            <th>Påmeldte</th>
+            <th>Beskrivelse</th>
+            <th>Opprettet</th>
+            <th>Godkjent</th>
+            <th>Godkjenn/Fjern</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
 
-foreach ($oppgaveListe as $oppgave) {
-	?>			<tr id="<?php echo $oppgave->getId(); ?>">
-				<td><?php echo $oppgave->getNavn(); ?></td>
-			  <td><?php echo $oppgave->getPrioritetId(); ?></td>
-				<td><?php echo $oppgave->getAnslagTimer(); ?></td>
-				<td><?php echo $oppgave->getAnslagPersoner(); ?></td>
-				<td><?php null == null ? '':''; ?></td>
-				<td><?php echo htmlspecialchars($oppgave->getBeskrivelse()); ?></td>
-        <td><?php echo $oppgave->getTidOppretta(); ?></td>
-        <td><?php echo $oppgave->getTidGodkjent() != null ? '<span title="Godkjent av ' . intern3\Bruker::medId($oppgave->getGodkjentBrukerId()) == null ? intern3\Bruker::medId($oppgave->getGodkjentId())->getPerson()->getFulltNavn() : intern3\Beboer::medId($oppgave->getGodkjentBrukerId())->getFulltNavn() . ' > ' . $oppgave->getTidGodkjent() . '</span>' : ''; ?></td>
-        <td><?php echo $oppgave->getGodkjent() > 0 ? '' : '<button onclick="godkjenn(' . $oppgave->getId() . ')">&#x2714;</button>'; ?> <button onclick="fjern(<?php $oppgave->getId(); ?>)">&#x2718;</button></td>
-			</tr>
-<?php
-}
-?>
-		</tbody>
-	</table>
+        foreach ($oppgaveListe as $oppgave) {
+            $id = $oppgave->getId();
+            $navn = $oppgave->getNavn();
+            $pri = $oppgave->getPrioritetId();
+            $timer = $oppgave->getAnslagTimer();
+            $personer = $oppgave->getAnslagPersoner();
+            $beskrivelse = $oppgave->getBeskrivelse();
+            $oppretta = $oppgave->getTidOppretta();
+            $tidgodkjent = $oppgave->getTidGodkjent();
+            $godkjent = $oppgave->getGodkjent();
+            $godkjentav = '';
+            //TODO FIX THIS SHIT MAN
+            if ($godkjent == -1) {
+                $godkjentav = intern3\Bruker::medId($oppgave->getGodkjentBrukerId()) == null ? intern3\Bruker::medId($oppgave->getGodkjentBruker())->getPerson()->getFulltNavn() : intern3\Beboer::medId($oppgave->getGodkjentBrukerId())->getFulltNavn();
+                //$godkjentav = $oppgave->getGodkjentBrukerId();
+            }
+
+            ?>
+            <tr id="<?php echo $id;?>">
+                <td><?php echo $navn; ?> </td>
+                <td><?php echo $pri; ?> </td>
+                <td><?php echo $timer; ?></td>
+                <td><?php echo $pri; ?></td>
+                <td><?php echo $personer; ?></td>
+                <td><?php echo $beskrivelse; ?></td>
+                <td><?php echo $oppretta; ?></td>
+                <td><?php echo $oppgave->getTidGodkjent() != null ? '<span title="Godkjent av ' . $godkjentav
+                        . '" > ' . $oppgave->getTidGodkjent() . '</span>' : ''; ?></td>
+
+                <td><?php if ($godkjent == 0) { ?>
+                    <button onclick="godkjenn(<?php echo $id; ?>)">Godkjenn</button><?php } ?>
+                    <button onclick="fjern(<?php echo $id; ?>)">Fjern</button>
+                </td>
+            </tr>
+
+            <?php
+        }
+
+
+        ?>
+        </tbody>
+    </table>
 </div>
 
 <?php
