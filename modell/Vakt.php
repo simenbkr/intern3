36881bd8845244986380a1c3f2b406d03ebca74f
simenbkr <simen@haitech.org>
2016-10-03 18:28:34 +0200

    La til nye funksjoner i Vakt-modellen for API mtp vakt-håndtering

--- modell/Vakt.php
+++ modell/Vakt.php
@@ -2,181 +2,244 @@
 
 namespace intern3;
 
-class Vakt {
+class Vakt
+{
 
-	private $id;
-	private $brukerId;
-	private $vakttype;
-	private $dato;
-	private $bekreftet;
-	private $autogenerert;
-	private $dobbelvakt;
+    private $id;
+    private $brukerId;
+    private $vakttype;
+    private $dato;
+    private $bekreftet;
+    private $autogenerert;
+    private $dobbelvakt;
 
-	// Latskap
-	private $bruker;
-	private $vaktbytte;
+    // Latskap
+    private $bruker;
+    private $vaktbytte;
 
-	public static function medId($id) {
-		$st = DB::getDB()->prepare('SELECT * FROM vakt WHERE id=:id;');
-		$st->bindParam(':id', $id);
-		$st->execute();
-		return self::init($st);
-	}
-	public static function medDatoVakttype($dato, $vakttype) {
-		$st = DB::getDB()->prepare('SELECT * FROM vakt WHERE dato=:dato AND vakttype=:vakttype;');
-		$st->bindParam(':dato', $dato);
-		$st->bindParam(':vakttype', $vakttype);
-		$st->execute();
-		return self::init($st);
-	}
-	private static function init(\PDOStatement $st) {
-		$rad = $st->fetch();
-		if ($rad == null) {
-			return null;
-		}
-		$instance = new self();
-		$instance->id = $rad['id'];
-		$instance->brukerId = $rad['bruker_id'];
-		$instance->vakttype = $rad['vakttype'];
-		$instance->dato = $rad['dato'];
-		$instance->bekreftet = $rad['bekreftet'];
-		$instance->autogenerert = $rad['autogenerert'];
-		$instance->dobbelvakt = $rad['dobbelvakt'];
-		return $instance;
-	}
-
-	public function getId() {
-		return $this->id;
-	}
-
-	public function getBrukerId() {
-		return $this->brukerId;
-	}
-
-	public function getBruker() {
-		if ($this->bruker == null) {
-			$this->bruker = Bruker::medId($this->brukerId);
-		}
-		return $this->bruker;
-	}
-
-	public function getVakttype() {
-		return $this->vakttype;
-	}
-
-	public function getDato() {
-		return $this->dato;
-	}
-
-	public function erLedig() {
-		return $this->brukerId==0 && $this->autogenerert;
-	}
-
-	public function getVaktbytte() {
-		if ($this->vaktbytte == null) {
-			$this->vaktbytte = Vaktbytte::medVaktId($this->id);
-		}
-		return $this->vaktbytte;
-	}
-
-	public function vilBytte() {
-		return $this->getVaktbytte() <> null;
-	}
-
-  public function erFerdig() {
-    $tid = strtotime($this->getDato());
-    $tid = strtotime('midnight', $tid);
-    switch ($this->getVakttype()) {
-      case '1':
-        $tid = strtotime('+8 hour', $tid);
-        break;
-      case '2':
-        $tid = strtotime('+13 hour', $tid);
-        break;
-      case '3':
-        $tid = strtotime('+19 hour', $tid);
-        break;
-      case '4':
-        $tid = strtotime('+1 hour', $tid);
-        break;
+    public static function medId($id)
+    {
+        $st = DB::getDB()->prepare('SELECT * FROM vakt WHERE id=:id;');
+        $st->bindParam(':id', $id);
+        $st->execute();
+        return self::init($st);
     }
-    return $tid <= $_SERVER['REQUEST_TIME'];
-  }
 
-  public static function antallVakter() {
-    $st = DB::getDB()->prepare('SELECT count(id) AS antall FROM vakt');
-    $st->execute();
-    $res = $st->fetch();
-    return $res['antall'];
-  }
+    public static function medDatoVakttype($dato, $vakttype)
+    {
+        $st = DB::getDB()->prepare('SELECT * FROM vakt WHERE dato=:dato AND vakttype=:vakttype;');
+        $st->bindParam(':dato', $dato);
+        $st->bindParam(':vakttype', $vakttype);
+        $st->execute();
+        return self::init($st);
+    }
 
-  public static function antallUfordelte() {
-    $st = DB::getDB()->prepare('SELECT count(id) AS antall FROM vakt WHERE bruker_id = 0;');
-    $st->execute();
-    $res = $st->fetch();
-    return $res['antall'];
-  }
+    private static function init(\PDOStatement $st)
+    {
+        $rad = $st->fetch();
+        if ($rad == null) {
+            return null;
+        }
+        $instance = new self();
+        $instance->id = $rad['id'];
+        $instance->brukerId = $rad['bruker_id'];
+        $instance->vakttype = $rad['vakttype'];
+        $instance->dato = $rad['dato'];
+        $instance->bekreftet = $rad['bekreftet'];
+        $instance->autogenerert = $rad['autogenerert'];
+        $instance->dobbelvakt = $rad['dobbelvakt'];
+        return $instance;
+    }
 
-  public static function antallUbekreftet() {
-    $st = DB::getDB()->prepare('SELECT count(id) AS antall FROM vakt WHERE bekreftet = 0 AND bruker_id != 0;');
-    $st->execute();
-    $res = $st->fetch();
-    return $res['antall'];
-  }
+    public function getId()
+    {
+        return $this->id;
+    }
 
-	public static function antallSkalSitteMedBrukerId($brukerId) {
-		$beboer = Beboer::medBrukerId($brukerId);
-		if ($beboer == null || !$beboer->erBeboer()) {
-			return 0;
-		}
-		$rolle = $beboer->getRolle();
-		return date('m') > 6 ? $rolle->getVakterH() : $rolle->getVakterV();
-	}
+    public function getBrukerId()
+    {
+        return $this->brukerId;
+    }
 
-  public static function antallHarSittetMedBrukerId($brukerId) {
-    $st = DB::getDB()->prepare('SELECT count(id) AS antall FROM vakt WHERE bruker_id=:brukerId AND vakt.dato < CURDATE();');
-    $st->bindParam(':brukerId', $brukerId);
-    $st->execute();
-    $res = $st->fetch();
-    return $res['antall'];
-  }
+    public function getBruker()
+    {
+        if ($this->bruker == null) {
+            $this->bruker = Bruker::medId($this->brukerId);
+        }
+        return $this->bruker;
+    }
 
-  public static function antallErOppsattMedBrukerId($brukerId) {
-    $st = DB::getDB()->prepare('SELECT count(id) AS antall FROM vakt WHERE bruker_id=:brukerId AND vakt.dato >= CURDATE()');
-    $st->bindParam(':brukerId', $brukerId);
-    $st->execute();
-    $res = $st->fetch();
-    return $res['antall'];
-  }
+    public function getVakttype()
+    {
+        return $this->vakttype;
+    }
 
-	public static function antallForsteMedBrukerId($brukerId) {
-		$st = DB::getDB()->prepare('SELECT count(id) AS antall FROM vakt WHERE bruker_id=:brukerId AND vakttype=\'1\'');
-		$st->bindParam(':brukerId', $brukerId);
-		$st->execute();
-		$res = $st->fetch();
-		return $res['antall'];
-	}
+    public function getDato()
+    {
+        return $this->dato;
+    }
 
-  public static function antallHarIgjenMedBrukerId($brukerId, $skalSitte) {
-    $antall = $skalSitte;
-    $antall -= Vakt::antallHarSittetMedBrukerId($brukerId);
-    return $antall;
-  }
+    public function erLedig()
+    {
+        return $this->brukerId == 0 && $this->autogenerert;
+    }
 
-  public static function antallIkkeOppsattMedBrukerId($brukerId, $skalSitte) {
-    $antall = $skalSitte;
-    $antall -= Vakt::antallHarSittetMedBrukerId($brukerId);
-    $antall -= Vakt::antallErOppsattMedBrukerId($brukerId);
-    return $antall;
-  }
+    public function getVaktbytte()
+    {
+        if ($this->vaktbytte == null) {
+            $this->vaktbytte = Vaktbytte::medVaktId($this->id);
+        }
+        return $this->vaktbytte;
+    }
 
-  public static function antallIkkeBekreftetMedBrukerId($brukerId) {
-    $st = DB::getDB()->prepare('SELECT count(id) AS antall FROM vakt WHERE bruker_id=:brukerId AND bekreftet = 0');
-    $st->bindParam(':brukerId', $brukerId);
-    $st->execute();
-    $res = $st->fetch();
-    return $res['antall'];
-  }
+    public function vilBytte()
+    {
+        return $this->getVaktbytte() <> null;
+    }
+
+    public function erFerdig()
+    {
+        $tid = strtotime($this->getDato());
+        $tid = strtotime('midnight', $tid);
+        switch ($this->getVakttype()) {
+            case '1':
+                $tid = strtotime('+8 hour', $tid);
+                break;
+            case '2':
+                $tid = strtotime('+13 hour', $tid);
+                break;
+            case '3':
+                $tid = strtotime('+19 hour', $tid);
+                break;
+            case '4':
+                $tid = strtotime('+1 hour', $tid);
+                break;
+        }
+        return $tid <= $_SERVER['REQUEST_TIME'];
+    }
+
+    public static function antallVakter()
+    {
+        $st = DB::getDB()->prepare('SELECT count(id) AS antall FROM vakt');
+        $st->execute();
+        $res = $st->fetch();
+        return $res['antall'];
+    }
+
+    public static function antallUfordelte()
+    {
+        $st = DB::getDB()->prepare('SELECT count(id) AS antall FROM vakt WHERE bruker_id = 0;');
+        $st->execute();
+        $res = $st->fetch();
+        return $res['antall'];
+    }
+
+    public static function antallUbekreftet()
+    {
+        $st = DB::getDB()->prepare('SELECT count(id) AS antall FROM vakt WHERE bekreftet = 0 AND bruker_id != 0;');
+        $st->execute();
+        $res = $st->fetch();
+        return $res['antall'];
+    }
+
+    public static function antallSkalSitteMedBrukerId($brukerId)
+    {
+        $beboer = Beboer::medBrukerId($brukerId);
+        if ($beboer == null || !$beboer->erBeboer()) {
+            return 0;
+        }
+        $rolle = $beboer->getRolle();
+        return date('m') > 6 ? $rolle->getVakterH() : $rolle->getVakterV();
+    }
+
+    public static function antallHarSittetMedBrukerId($brukerId)
+    {
+        $st = DB::getDB()->prepare('SELECT count(id) AS antall FROM vakt WHERE bruker_id=:brukerId AND vakt.dato < CURDATE();');
+        $st->bindParam(':brukerId', $brukerId);
+        $st->execute();
+        $res = $st->fetch();
+        return $res['antall'];
+    }
+
+    public static function antallErOppsattMedBrukerId($brukerId)
+    {
+        $st = DB::getDB()->prepare('SELECT count(id) AS antall FROM vakt WHERE bruker_id=:brukerId AND vakt.dato >= CURDATE()');
+        $st->bindParam(':brukerId', $brukerId);
+        $st->execute();
+        $res = $st->fetch();
+        return $res['antall'];
+    }
+
+    public static function antallForsteMedBrukerId($brukerId)
+    {
+        $st = DB::getDB()->prepare('SELECT count(id) AS antall FROM vakt WHERE bruker_id=:brukerId AND vakttype=\'1\'');
+        $st->bindParam(':brukerId', $brukerId);
+        $st->execute();
+        $res = $st->fetch();
+        return $res['antall'];
+    }
+
+    public static function antallHarIgjenMedBrukerId($brukerId, $skalSitte)
+    {
+        $antall = $skalSitte;
+        $antall -= Vakt::antallHarSittetMedBrukerId($brukerId);
+        return $antall;
+    }
+
+    public static function antallIkkeOppsattMedBrukerId($brukerId, $skalSitte)
+    {
+        $antall = $skalSitte;
+        $antall -= Vakt::antallHarSittetMedBrukerId($brukerId);
+        $antall -= Vakt::antallErOppsattMedBrukerId($brukerId);
+        return $antall;
+    }
+
+    public static function antallIkkeBekreftetMedBrukerId($brukerId)
+    {
+        $st = DB::getDB()->prepare('SELECT count(id) AS antall FROM vakt WHERE bruker_id=:brukerId AND bekreftet = 0');
+        $st->bindParam(':brukerId', $brukerId);
+        $st->execute();
+        $res = $st->fetch();
+        return $res['antall'];
+    }
+
+    public static function slettVakt($vaktId){
+        $st = DB::getDB()->prepare('DELETE FROM vakt WHERE id=:id');
+        $st->bindParam(':id',$vaktId);
+        $st->execute();
+    }
+
+    public static function setDobbelVakt($vaktId){
+        $st = DB::getDB()->prepare('UPDATE vakt SET dobbelvakt=1 WHERE id=:id');
+        $st->bindParam(':id',$vaktId);
+        $st->execute();
+    }
+
+    public static function byttVakt($vaktId1,$vaktId2){
+        //Hent ut relevant info.
+        $st = DB::getDB()->prepare('SELECT bruker_id FROM vakt WHERE id=:id');
+        $st->bindParam(':id',$vaktId1);
+        $st->execute();
+        $forste = $st->fetchColumn();
+
+        $st = DB::getDB()->prepare('SELECT bruker_id FROM vakt WHERE id=:id');
+        $st->bindParam(':id',$vaktId2);
+        $st->execute();
+        $andre = $st->fetchColumn();
+
+        //Oppdatere de to.
+        $st = DB::getDB()->prepare('UPDATE vakt SET bruker_id=:bruker_id WHERE id=:id');
+        $st->bindParam(':bruker_id',$andre['bruker_id']);
+        $st->bindParam(':id',$vaktId1);
+        $st->execute();
+
+        $st = DB::getDB()->prepare('UPDATE vakt SET bruker_id=:bruker_id WHERE id=:id');
+        $st->bindParam(':bruker_id',$forste['bruker_id']);
+        $st->bindParam(':id',$vaktId2);
+        $st->execute();
+    }
+
+
 }
 
 ?>
