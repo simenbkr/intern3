d30f6eff33ac0dff794b49504c655dbc8148f9b5
simenbkr <simen@haitech.org>
2016-10-03 13:25:53 +0200

    La til ukerapport i VaktsjefCTRL igjen..

--- kontroller/UtvalgVaktsjefCtrl.php
+++ kontroller/UtvalgVaktsjefCtrl.php
@@ -2,63 +2,92 @@
 
 namespace intern3;
 
-class UtvalgVaktsjefCtrl extends AbstraktCtrl {
-	public function bestemHandling() {
-		$aktueltArg = $this->cd->getAktueltArg();
-		switch ($aktueltArg) {
-      // case 'vaktstyring':
-      //   $valgtCtrl = new UtvalgVaktsjefVaktstyringCtrl($this->cd->skiftArg());
-      //   $valgtCtrl->bestemHandling();
-      //   break;
-      case 'generer':
-        $valgtCtrl = new UtvalgVaktsjefGenererCtrl($this->cd->skiftArg());
-        $valgtCtrl->bestemHandling();
-        break;
-			case 'vaktoversikt':
-				$beboerListe = BeboerListe::harVakt();
-				$antallVakter = Vakt::antallVakter();
-				$antallUfordelte = Vakt::antallUfordelte();
-				$antallUbekreftet = Vakt::antallUbekreftet();
-				$dok = new Visning($this->cd);
-				$dok->set('beboerListe', $beboerListe);
-				$dok->set('antallVakter', $antallVakter);
-				$dok->set('antallUfordelte', $antallUfordelte);
-				$dok->set('antallUbekreftet', $antallUbekreftet);
-				$dok->vis('utvalg_vaktsjef_vaktoversikt.php');
-				break;
-      case 'vaktstyring':
-        $beboerListe = BeboerListe::harVakt();
-        $torild = Ansatt::medId(1);
-        $dok = new Visning($this->cd);
-        $dok->set('torild', $torild);
-        $dok->set('denneUka', @date('W'));
-        $dok->set('detteAret', @date('Y'));
-        $dok->set('beboerListe', $beboerListe);
-        $dok->vis('utvalg_vaktsjef_vaktstyring.php');
-        break;
-      case 'vaktstyring_settvakt':
-        $beboer = Beboer::medId($this->cd->getArg($this->cd->getAktuellArgPos()+1));
-        if ($beboer == NULL) {
-          exit();
+class UtvalgVaktsjefCtrl extends AbstraktCtrl
+{
+    public function bestemHandling()
+    {
+        $aktueltArg = $this->cd->getAktueltArg();
+        switch ($aktueltArg) {
+            // case 'vaktstyring':
+            //   $valgtCtrl = new UtvalgVaktsjefVaktstyringCtrl($this->cd->skiftArg());
+            //   $valgtCtrl->bestemHandling();
+            //   break;
+            case 'generer':
+                $valgtCtrl = new UtvalgVaktsjefGenererCtrl($this->cd->skiftArg());
+                $valgtCtrl->bestemHandling();
+                break;
+            case 'vaktoversikt':
+                $beboerListe = BeboerListe::harVakt();
+                $antallVakter = Vakt::antallVakter();
+                $antallUfordelte = Vakt::antallUfordelte();
+                $antallUbekreftet = Vakt::antallUbekreftet();
+                $dok = new Visning($this->cd);
+                $dok->set('beboerListe', $beboerListe);
+                $dok->set('antallVakter', $antallVakter);
+                $dok->set('antallUfordelte', $antallUfordelte);
+                $dok->set('antallUbekreftet', $antallUbekreftet);
+                $dok->vis('utvalg_vaktsjef_vaktoversikt.php');
+                break;
+            case 'vaktstyring':
+                $beboerListe = BeboerListe::harVakt();
+                $torild = Ansatt::medId(1);
+                $dok = new Visning($this->cd);
+                $dok->set('torild', $torild);
+                $dok->set('denneUka', @date('W'));
+                $dok->set('detteAret', @date('Y'));
+                $dok->set('beboerListe', $beboerListe);
+                $dok->vis('utvalg_vaktsjef_vaktstyring.php');
+                break;
+            case 'vaktstyring_settvakt':
+                $beboer = Beboer::medId($this->cd->getArg($this->cd->getAktuellArgPos() + 1));
+                if ($beboer == NULL) {
+                    exit();
+                } else {
+                    $dok = new Visning($this->cd);
+                    $dok->set('beboer', $beboer);
+                    $dok->vis('utvalg_vaktsjef_vaktstyring_settvakt.php');
+                }
+                break;
+            case 'vaktstyring_modal':
+                $beboerListe = BeboerListe::harVakt();
+                $dok = new Visning($this->cd);
+                $dok->set('beboerListe', $beboerListe);
+                $dok->vis('utvalg_vaktsjef_vaktstyring_modal.php');
+                break;
+            case 'ukerapport':
+                $Uke = $this->cd->getArg($this->cd->getAktuellArgPos() + 1);
+                $Aar = $this->cd->getArg($this->cd->getAktuellArgPos() + 2);
+                if (is_numeric($Uke)) {
+                    $krysseinstans = new Journal($Uke, $Aar);
+                } else {
+                    $krysseinstans = new Journal();
+                }
+                $dok = new Visning($this->cd);
+                $dok->set('krysseting', $krysseinstans->getUkeKryss());
+                $dok->set('journal', $krysseinstans->getKrysseInfo());
+                $dok->vis('utvalg_vaktsjef_ukesrapport.php');
+                break;
+            case 'ukerapport_tabell':
+                $Uke = $this->cd->getArg($this->cd->getAktuellArgPos() + 1);
+                $Aar = $this->cd->getArg($this->cd->getAktuellArgPos() + 2);
+                if (is_numeric($Uke)) {
+                    $krysseinstans = new Journal($Uke, $Aar);
+                } else {
+                    $krysseinstans = new Journal();
+                }
+                $dok = new Visning($this->cd);
+                $dok->set('krysseting', $krysseinstans->getUkeKryss());
+                $dok->set('journal', $krysseinstans->getKrysseInfo());
+                $dok->vis('utvalg_vaktsjef_ukesrapport_tabell.php');
+                break;
+
+
+            default:
+                $dok = new Visning($this->cd);
+                $dok->vis('utvalg_vaktsjef.php');
+                break;
         }
-        else {
-          $dok = new Visning($this->cd);
-          $dok->set('beboer', $beboer);
-          $dok->vis('utvalg_vaktsjef_vaktstyring_settvakt.php');
-        }
-        break;
-      case 'vaktstyring_modal':
-        $beboerListe = BeboerListe::harVakt();
-        $dok = new Visning($this->cd);
-        $dok->set('beboerListe', $beboerListe);
-        $dok->vis('utvalg_vaktsjef_vaktstyring_modal.php');
-        break;
-			default:
-				$dok = new Visning($this->cd);
-				$dok->vis('utvalg_vaktsjef.php');
-				break;
-		}
-	}
+    }
 }
 
 ?>
