0fa8e9194706317c424baa0a7d825b4265c28c1f
simenbkr <simen@haitech.org>
2016-10-09 19:38:33 +0200

    La til tabell for epost-preferanser. Disse er opt-out.

--- www/migrer.php
+++ www/migrer.php
@@ -601,6 +601,34 @@ while ($rad = $hent->fetch()) {
 
 /* Migrering av oppgave, slutt */
 
+
+/* Oppsett av epost_preferanser start */
+
+$beboerliste = BeboerListe::alle();
+
+$st = DB::getDB()->prepare('CREATE TABLE epost_pref (
+beboer_id INT(10) UNSIGNED NOT NULL,
+tildelt TINYINT(1) NOT NULL DEFAULT 1,
+snart_vakt TINYINT(1) NOT NULL DEFAULT 1,
+bytte TINYINT(1) NOT NULL DEFAULT 1,
+utleie TINYINT(1) NOT NULL DEFAULT 1,
+barvakt TINYINT(1) NOT NULL DEFAULT 1
+)');
+$st->execute();
+
+foreach($beboerliste as $beboer){
+	$iden = $beboer->getId();
+	$st = DB::getDB()->prepare('INSERT INTO epost_pref(beboer_id,tildelt,snart_vakt,bytte,utleie,barvakt
+	) VALUES(
+	:beboer_id,1,1,1,1,1)');
+	$st->bindParam(':beboer_id',$iden);
+	$st->execute();
+}
+
+/* Oppsett av epost_pref slutt */
+
+
+
 /* Migrering av kryssejournal start */
 $hentKrysseliste = pg_query('SELECT * FROM krysseliste ORDER BY dato');
 
