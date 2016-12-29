6029ed9f114beed070e6994d5f88affced5b8f80
simenbkr <simen@haitech.org>
2016-12-29 02:39:36 +0100

    Kan ikke skrive. Div. bugfixes..

--- www/index.php
+++ www/index.php
@@ -4,12 +4,13 @@ setlocale(LC_ALL, 'nb_NO.utf-8');
 date_default_timezone_set('Europe/Oslo');
 ini_set('session.cookie_httponly',1);
 ini_set('session.use_only_cookies',1);
+ini_set('session.gc_maxlifetime', 36000000);
 $arg = isset($_GET['a']) ? explode('/', $_GET['a']) : array();
 
 require_once('../ink/autolast.php');
 
 header('Content-Type: text/html; charset=utf-8');
-
+session_start();
 $cd = new intern3\CtrlData($arg);
 $ctrl = new intern3\HovedCtrl($cd);
 $ctrl->bestemHandling();
