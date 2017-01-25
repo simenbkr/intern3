a2950e9e846a8097aa1414bec69811d2bb991f54
simenbkr <simen@haitech.org>
2017-01-25 14:14:10 +0100

    Bugfixes

--- visning/kjeller_svinn.php
+++ visning/kjeller_svinn.php
@@ -26,7 +26,7 @@ require_once('topp.php');
     <h1>Kjellermester » Registrer svinn</h1>
     <p>[ <a href="<?php echo $cd->getBase(); ?>kjeller/admin">Vinadministrasjon</a> ] [ <a href="<?php echo $cd->getBase(); ?>kjeller/leggtil">Legg til vin</a> ] [ <a href="<?php echo $cd->getBase(); ?>kjeller/add_type">Vintyper</a> ]
         [ <a href="<?php echo $cd->getBase(); ?>kjeller/pafyll">Påfyll</a> ] [ <a href="<?php echo $cd->getBase(); ?>kjeller/lister">Lister</a> ]
-        [ <a href="<?php echo $cd->getBase(); ?>kjeller/regning">Regning</a> ] [ Svinn ]</p>
+        [ <a href="<?php echo $cd->getBase(); ?>kjeller/regning">Regning</a> ] [ Svinn ] [ <a href="<?php echo $cd->getBase(); ?>kjeller/lister/beboere_vin">Fakturer</a> ]</p>
     <hr>
     <div class="col-md-12">
         <form action="" method="post" enctype="multipart/form-data">
