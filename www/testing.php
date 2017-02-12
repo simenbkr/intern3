dce4f8ffc4db5f42eec12e20ea204e6fa9293042
simenbkr <simen@haitech.org>
2017-02-12 21:07:18 +0100

    Merget alt. Lagt til egne endringer.

--- www/testing.php
+++ www/testing.php
@@ -4,5 +4,6 @@ require_once ('../ink/phpqrcode.php');
 require_once("../ink/autolast.php");
 setlocale(LC_ALL, 'nb_NO.utf-8');date_default_timezone_set('Europe/Oslo');
 
+var_dump(Bruker::medEpost("stineaa1@hotmail.com")->getPerson());
 
 ?>
