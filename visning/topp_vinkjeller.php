147b805a7dc318762ba1e3059154f52aa4ca0f0e
simenbkr <simen@haitech.org>
2017-07-13 15:04:05 +0200

    Fiksa vinkjeller-topp.

--- visning/topp_vinkjeller.php
+++ visning/topp_vinkjeller.php
@@ -11,10 +11,9 @@
         </div>
         <div id="navbar" class="navbar-collapse collapse">
             <ul class="nav navbar-nav">
-                <li><a href="?a=vinkjeller/kryssing">Kryssing</a></li>
+                <li><a href="?a=vinkjeller/kryssing/navn">Navn</a></li>
+                <li><a href="?a=vinkjeller/kryssing/type">Type</a></li>
                 <li><a href="?a=vinkjeller/regler">Regler</a></li>
-                <li><a href="?a=vinkjeller/">???</a></li>
-                <li><a href="?a=vinkjeller/">???</a></li>
             </ul>
         </div>
     </div>
