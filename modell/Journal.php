1ae7f3a339a77fbd9b11ea13d01ad946b15d1220
simenbkr <simen@haitech.org>
2017-09-20 15:03:36 +0200

    Enda en liten fiks på Journal-modell.

--- modell/Journal.php
+++ modell/Journal.php
@@ -145,8 +145,14 @@ class Journal
                         'dato' => $aktuell_vaktsesjon->getDato()
                     );
                 } else {
+
+                    $vakthavende = Vakt::medDatoVakttype($aktuell_vaktsesjon->getDato(), $aktuell_vaktsesjon->getVaktnr());
+                    if($vakthavende != null && $vakthavende->getBruker() != null){
+                        $vakthavende = $vakthavende->getBruker()->getPerson();
+                    }
+
                     $denne_vakta = array(
-                        'vakthavende' => null,
+                        'vakthavende' => $vakthavende,
                         'vaktnr' => $aktuell_vaktsesjon->getVaktnr(),
                         'dato' => $aktuell_vaktsesjon->getDato()
                     );
@@ -162,65 +168,7 @@ class Journal
         }
         return $endelig_array;
     }
-
-    /*public function getKrysseInfo(){
-        $info_arr = array();
-        $perioden = self::getPeriode();
-        $start = date('Y-m-d',strtotime('-1 week',strtotime($perioden['fra'])));
-        $slutt = date('Y-m-d',strtotime('-1 week',strtotime($perioden['til'])));
-        //Henter ut siste kryssing/vaktbytte/overføring fra forrige periode.
-        $st = DB::getDB()->prepare('SELECT * FROM journal WHERE dato>=:start AND dato <=:slutt ORDER BY dato DESC');
-        $st->bindParam(':start', $start);
-        $st->bindParam(':slutt', $slutt);
-        $st->execute();
-        //$forrige = $st->fetchAll()[0];
-
-        //Henter ut hele journalen for nåværende uke.
-        $st = DB::getDB()->prepare('SELECT * FROM journal WHERE dato>=:start AND dato<=:slutt ORDER BY dato ASC');
-        $st->bindParam(':start',$perioden['fra']);
-        $st->bindParam(':slutt',$perioden['til']);
-        $st->execute();
-
-        $journalen = $st->fetchAll();
-
-        $endelig_array = array();
-
-        $indeks = 0;
-        foreach($journalen as $vakt){
-            $denne_vakta = array();
-            $vakten = ($vakt['beboer_id'] > 1 ? Beboer::medId($vakt['beboer_id']) : Ansatt::getSisteAnsatt());
-            $denne_vakta['vakthavende'] = $vakten->getFulltNavn();
-            $denne_vakta['vaktnr'] = $vakt['vakt'];
-            $denne_vakta['dato'] = $vakt['dato'];
-
-            $denne_vakta['ol_mottatt'] = $vakt['ol_mottatt'];
-            $denne_vakta['ol_pafyll'] = $vakt['ol_pafyll'];
-            $denne_vakta['ol_avlevert'] = $vakt['ol_avlevert'];
-            $denne_vakta['ol_utavskap'] = $vakt['ol_utavskap'];
-            $denne_vakta['ol_svinn'] = $vakt['ol_mottatt']+$vakt['ol_pafyll']-$vakt['ol_utavskap']-$vakt['ol_avlevert'];
-
-            $denne_vakta['cid_mottatt'] = $vakt['cid_mottatt'];
-            $denne_vakta['cid_pafyll'] = $vakt['cid_pafyll'];
-            $denne_vakta['cid_avlevert'] = $vakt['cid_avlevert'];
-            $denne_vakta['cid_utavskap'] = $vakt['cid_utavskap'];
-            $denne_vakta['cid_svinn'] =  $vakt['cid_mottatt']+$vakt['cid_pafyll']-$vakt['cid_utavskap']-$vakt['cid_avlevert'];
-
-            $denne_vakta['carls_mottatt'] = $vakt['carls_mottatt'];
-            $denne_vakta['carls_pafyll'] = $vakt['carls_pafyll'];
-            $denne_vakta['carls_avlevert'] = $vakt['carls_avlevert'];
-            $denne_vakta['carls_utavskap'] = $vakt['carls_utavskap'];
-            $denne_vakta['carls_svinn'] =  $vakt['carls_mottatt']+$vakt['carls_pafyll']-$vakt['carls_utavskap']-$vakt['carls_avlevert'];
-
-            $denne_vakta['rikdom_mottatt'] = $vakt['rikdom_mottatt'];
-            $denne_vakta['rikdom_pafyll'] = $vakt['rikdom_pafyll'];
-            $denne_vakta['rikdom_avlevert'] = $vakt['rikdom_avlevert'];
-            $denne_vakta['rikdom_utavskap'] = $vakt['rikdom_utavskap'];
-            $denne_vakta['rikdom_svinn'] = $vakt['rikdom_mottatt']+$vakt['rikdom_pafyll']-$vakt['rikdom_utavskap']-$vakt['rikdom_avlevert'];
-
-            $endelig_array[] = $denne_vakta;
-        }
-        return $endelig_array;
-    }*/
+    
 }
 
 ?>
