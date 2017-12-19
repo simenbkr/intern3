7bf2d287730b3a2c17184809e7dd6899ccf9bbfc
simen <simen@haitech.org>
2017-12-19 14:04:22 +0100

    La til errorhandlers.

---
+++ ink/errorhandlers.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace intern3;
+
+
+function errorHandler($error_level, $error_message, $error_file, $error_line, $error_context)
+{
+    $error = "lvl: " . $error_level . " | msg:" . $error_message . " | file:" . $error_file . " | ln:" . $error_line;
+    switch ($error_level) {
+        case E_ERROR:
+        case E_CORE_ERROR:
+        case E_COMPILE_ERROR:
+        case E_PARSE:
+            mylog($error, "fatal");
+            break;
+        case E_USER_ERROR:
+        case E_RECOVERABLE_ERROR:
+            mylog($error, "error");
+            break;
+        case E_WARNING:
+        case E_CORE_WARNING:
+        case E_COMPILE_WARNING:
+        case E_USER_WARNING:
+            mylog($error, "warn");
+            break;
+        case E_NOTICE:
+        case E_USER_NOTICE:
+            mylog($error, "info");
+            break;
+        case E_STRICT:
+            mylog($error, "debug");
+            break;
+        default:
+            mylog($error, "warn");
+    }
+}
+
+function shutdownHandler() //will be called when php script ends.
+{
+    $lasterror = error_get_last();
+    switch ($lasterror['type']) {
+        case E_ERROR:
+        case E_CORE_ERROR:
+        case E_COMPILE_ERROR:
+        case E_USER_ERROR:
+        case E_RECOVERABLE_ERROR:
+        case E_CORE_WARNING:
+        case E_COMPILE_WARNING:
+        case E_PARSE:
+            $error = "[SHUTDOWN] lvl:" . $lasterror['type'] . " | msg:" . $lasterror['message'] . " | file:" . $lasterror['file'] . " | ln:" . $lasterror['line'];
+            mylog($error, "fatal");
+            //$cd = new CtrlData($arg = isset($_GET['a']) ? explode('/', $_GET['a']) : array());
+            //$visning = new Visning($cd);
+            //$visning->set('skjulMeny',1);
+            //$visning->set('url', '?a=feil');
+            //$visning->vis('redirect.php');
+            exit();
+    }
+}
+
+set_error_handler("\intern3\\errorHandler", E_ALL | E_STRICT);
+register_shutdown_function("\intern3\\shutdownHandler");
+
+function mylog($error, $errlvl)
+{
+
+    switch ($errlvl) {
+        case 'fatal':
+            Epost::sendEpost('data@singsaker.no', 'Fatal error på internsida', $error);
+            break;
+        case 'error':
+            Epost::sendEpost('data@singsaker.no', 'Error på internsida', $error);
+            break;
+    }
+}
+?>
--- www/index.php
+++ www/index.php
@@ -7,10 +7,15 @@ ini_set('session.cookie_httponly',1);
 ini_set('session.use_only_cookies',1);
 ini_set('session.gc_maxlifetime', 36000000);
 $arg = isset($_GET['a']) ? explode('/', $_GET['a']) : array();
+
+require_once('../ink/errorhandlers.php');
 require_once ('../ink/phpqrcode.php');
 require_once('../ink/autolast.php');
+
 session_set_cookie_params(3600*24*100*100,"/");
+
 header('Content-Type: text/html; charset=utf-8');
+
 session_start();
 $cd = new intern3\CtrlData($arg);
 $ctrl = new intern3\HovedCtrl($cd);
