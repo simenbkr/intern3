bae8c0ef5daf817c8fb6a2416b3b9973c3acf82b
simen <simen@haitech.org>
2018-01-06 02:51:48 +0100

    Rettet opp til riktig datoformat i Epostklassen.

--- modell/Epost.php
+++ modell/Epost.php
@@ -110,7 +110,7 @@ class Epost
         $headers .= "Return-Path: no-reply@mail.singsaker.no\r\n";
         $headers .= "X-Mailer: PHP/" . phpversion() . "\r\n";
         $headers .= "MIME-Version: 1.0" . "\r\n";
-        $headers .= "Date: " . date('H:i:s, d M Y') . " UTC +0000\r\n";
+        $headers .= "Date: " . date('r') . "\r\n";
         $headers .= "Content-type:text/html;charset=UTF-8" . "\r\n";
 
         $tittelen = (isset($tittel) && $tittel != null) ? $tittel : "[INTERN] Du har fått en melding fra intern.singsaker.no";
