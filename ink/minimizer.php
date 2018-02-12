21b05679cc27e746219429daeeea0d0c562852ce
simen <simen@haitech.org>
2018-02-12 10:37:56 +0100

    La til output-buffering med gzip.

---
+++ ink/minimizer.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace intern3;
+
+require_once ('autolast_absolute.php');
+
+function sanitize_output($buffer) {
+
+    $search = array(
+        '/\>[^\S ]+/s',     // strip whitespaces after tags, except space
+        '/[^\S ]+\</s',     // strip whitespaces before tags, except space
+        '/(\s)+/s',         // shorten multiple whitespace sequences
+        '/<!--(.|\s)*?-->/' // Remove HTML comments
+    );
+
+    $replace = array(
+        '>',
+        '<',
+        '\\1',
+        ''
+    );
+
+    $buffer = preg_replace($search, $replace, $buffer);
+
+    return $buffer;
+}
+
+ob_start("sanitize_output");
