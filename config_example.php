1cfe7ae2b397adf51875325855c8f7b3880f0f24
simen <simen@haitech.org>
2018-10-09 11:44:26 +0200

    Oppdater eksempelkonfig til 'tilregnelige' defaults.

--- config_example.php
+++ config_example.php
@@ -3,8 +3,8 @@
 namespace intern3;
 
 define('PATH', __DIR__);
-define('DB_USER', "marksome");
-define('DB_PW', "123456");
+define('DB_USER', "intern3");
+define('DB_PW', "intern3");
 define('DB_DOMAIN', 'localhost');
 
 if($_SERVER['SERVER_NAME'] == 'intern.singsaker.no' || $_SERVER['SERVER_NAME'] == 'dobbel.singsaker.no') {
@@ -12,3 +12,16 @@ if($_SERVER['SERVER_NAME'] == 'intern.singsaker.no' || $_SERVER['SERVER_NAME'] =
 } else {
     define('DB_NAME', 'intern3_dev');
 }
+
+define('SING_ALLE', 'sing-alle@singsaker.no');
+define('SING_SLARV', 'sing-slarv@singsaker.no');
+define('SING_JENTER', 'sing-jenter@singsaker.no');
+define('SING_GUTTER', 'sing-gutter@singsaker.no');
+define('SING_KORR', 'sing-korr@singsaker.no');
+define('SING_MISJ', 'sing-misj@singsaker.no');
+define('SING_VETERAN', 'sing-veteran@singsaker.no');
+
+
+define("MAIL_LISTS", array(SING_ALLE, SING_SLARV, SING_JENTER, SING_GUTTER));
+define("PUBLIC_MAIL", array(SING_ALLE, SING_SLARV));
+define("REGIBILDER","regibilder/");
