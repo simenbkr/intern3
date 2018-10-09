a6f689916380c4b5a902833494ee2df368b17d35
simen <simen@haitech.org>
2018-10-09 12:11:25 +0200

    La til basic databasefil (intet skummelt/hemmelig i denne). Endra på installskript.

--- install.bash
+++ install.bash
@@ -7,10 +7,9 @@ echo "Installerer Composer"
 curl -sS https://getcomposer.org/installer | php && php composer.phar install
 
 echo "Setter opp database"
-mysql -u intern3 -p intern3 < db/2017-09-28-intern3.sql
-echo "Kjører PHP-skript for å ferdiggjøre database"
-cd ink/misc && for f in *.php; php "$f"; done
+mysql -u intern3 -p intern3_dev < intern3_basic.sql
 
+echo "Du kan nå logge inn med e-post test@testesen.no, og passord testetest."
 
 
 
