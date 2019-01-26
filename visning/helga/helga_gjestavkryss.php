2c8785006202e6a38c751501bfc6c3da3e743cfa
simenbkr <simen@haitech.org>
2019-01-26 14:09:30 +0100

    Fikset Helga-inngang enda mer.

--- visning/helga/helga_gjestavkryss.php
+++ visning/helga/helga_gjestavkryss.php
@@ -6,18 +6,32 @@
                 <td>
                     <b><?php echo ($beboerliste[$beboers_id] != null) ? $beboerliste[$beboers_id]->getFulltNavn() : ''; ?></b>
                 </td>
+                <td></td>
             </tr>
             <?php
             foreach ($beboers_gjester as $gjest) {
                 /* @var \intern3\HelgaGjest $gjest */
                 $klassen = $gjest->getInne() == 0 ? 'bg-warning' : 'bg-success';
                 $checked = $gjest->getInne() == 0 ? 'none' : 'checked=\"checked\"';
+                $class = $gjest->getInne() == 0 ? 'btn-danger' : 'btn-info';
                 $verdi = $gjest->getInne() == 0 ? 1 : 0;
-                echo "<tr class=\"$klassen\" id='" . $gjest->getId() .
-                    "' onclick='registrer(" . $gjest->getId() . ",$verdi)'>
-                        <td>" . $gjest->getNavn() . "</td>";
+                $tekst = $gjest->getInne() == 0 ? 'Registrer' : 'Fjern';
+                ?>
 
-                echo "<td><input id=" . $gjest->getId() . "-knapp type=\"checkbox\"  value=\"" . $gjest->getId() . "\" onclick='registrer(" . $gjest->getId() . ",$verdi)' $checked></td></tr>";
+                <tr id="<?php echo $gjest->getId(); ?>" class="<?php echo $klassen; ?>">
+                    <td id="<?php echo $gjest->getId(); ?>-navn"
+                        onclick="registrer(<?php echo $gjest->getId(); ?>, <?php echo $verdi; ?>)">
+                        <?php echo $gjest->getNavn(); ?>
+                    </td>
+
+                    <td>
+                        <button id="<?php echo $gjest->getId(); ?>-knapp" class="btn <?php echo $class; ?>"
+                                onclick="registrer(<?php echo $gjest->getId(); ?>, <?php echo $verdi; ?>)">
+                            <?php echo $tekst; ?>
+                        </button>
+                    </td>
+                </tr>
+                <?php
             }
             ?></table>
         <?php
