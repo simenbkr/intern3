011e7bf72f4a54fca5bfcd38387372f6a59dc5b3
simenbkr <simen@haitech.org>
2019-08-08 16:58:12 +0200

    Just add a cronjob lol

--- ink/arkiver_gamle_oppgaver.php
+++ ink/arkiver_gamle_oppgaver.php
@@ -1,9 +1,16 @@
 <?php
 
-
 namespace intern3;
 
 require_once("/var/www/intern.singsaker.no/ink/autolast_absolute.php");
+
+$year = date('Y');
+$running_dates = array("$year-08-08", "$year-01-01");
+$now = date('Y-m-d');
+
+if(!in_array($now, $running_dates)) {
+    exit("Ikke korrekt tidspunkt. Lukker");
+}
 
 $oppgaveListe = OppgaveListe::ikkeGodkjente();
 
@@ -11,3 +18,5 @@ foreach($oppgaveListe as $oppgave) {
     /* @var \intern3\Oppgave $oppgave */
     $oppgave->arkiver();
 }
+
+exit("Arkiverte gamle oppgaver");
