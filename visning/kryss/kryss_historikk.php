700aed5f52328df330b3c01b3149e25448772d95
simenbkr <simen@haitech.org>
2019-08-25 20:20:53 +0200

    A surprise button appears.

--- visning/kryss/kryss_historikk.php
+++ visning/kryss/kryss_historikk.php
@@ -18,6 +18,9 @@ require_once(__DIR__ . '/../static/topp.php');
 </script>
 <div class="col-md-12">
     <h1>Kryss &raquo; Historikk</h1>
+
+    [ Kryss historikk] | [ <a href="?a=kryss/statistikk">Kryssestatistikk</a> ]
+
     <?php /*<p>[ <a href="?a=kryss">Nylig kryssing</a> ] [ <a href="?a=kryss/historikk">Historikk</a> ] [ <a href="?a=kryss/statistikk">Statistikk</a> ]</p>*/ ?>
     <hr>
 </div>
