9260b75c2d133526fe03fa7eb2f5618784191dd9
simenbkr <simen@haitech.org>
2020-01-11 14:09:05 +0100

    Legg til støtte for preloading.

--- ink/preloader.php
+++ ink/preloader.php
@@ -1,1 +1,17 @@
 <?php
+
+namespace intern3;
+
+require_once(__DIR__ . '/../config.php');
+
+foreach (array('modell', 'visning', 'kontroller') as $mappe) {
+    $iterator = new \DirectoryIterator(PATH . '/' . $mappe . '/');
+
+    foreach ($iterator as $fileinfo) {
+        if($fileinfo->isDot() || $fileinfo->isDir()) {
+            continue;
+        }
+        opcache_compile_file($fileinfo->getPath() . '/' . $fileinfo->getFilename());
+    }
+
+}
