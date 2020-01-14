e5823f79c219ee22d3071874aff0b8e18a1993a5
kev <kevinnor1997@gmail.com>
2020-01-14 21:17:24 +0100

    la til sql-spørring for å legge til kolonne i drikke-tabellen

---
+++ ink/misc/change_drikke.php
@@ -0,0 +1,25 @@
+<?php
+namespace intern3;
+
+require_once("../autolast_absolute.php");
+setlocale(LC_ALL, 'nb_NO.utf-8');
+date_default_timezone_set('Europe/Oslo');
+
+
+set_time_limit(999999999);
+$start = time();
+echo "Endring av drikke-tabell ved " . date('Y-m-d H:i:s');
+echo "\n";
+
+$sql = "ALTER TABLE `drikke` ADD `drikke` VARCHAR(255) NULL AFTER `farge`";
+
+
+DB::getDB()->query($sql);
+
+echo "FIN\n";
+$slutt = time();
+$tid = $slutt - $start;
+echo "Brukte tid: $tid sekunder\n";
+echo "Ferdig " . date('Y-m-d H:i:s');
+echo "\n";
+?>
